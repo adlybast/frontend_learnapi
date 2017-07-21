a91e71d62d7a6954199d6f8494e6444da57fc439
adrianus johan <adrianus@go-jek.com>
2017-07-21 22:23:05 +0700

    modified

---
+++ img/api_conf.js
@@ -0,0 +1,19 @@
+var baseUrl = "http://localhost/backend_restapi/index.php";
+var login = "/auth/login";
+var logout = "/auth/logout";
+
+var addStudent = "/student/create";
+var updateStudent = "/student/update/";
+var deleteStudent = "/student/delete/";
+var getStudent = "/student/detail/";
+var getallStudent = "/student";
+var getStudentAvailable = "/student/available"
+var kickStudent = "/student/kick/"
+var assignStudent = "/student/assign/"
+
+var addClass = "/class_controller/create";
+var updateClass = "/class_controller/update/";
+var deleteClass = "/class_controller/delete/";
+var getClass = "/class_controller/detail/";
+var getallClass = "/class_controller";
+var getClasslist = "/class_controller/class_list/";
---
+++ js/api_conf.js
@@ -0,0 +1,19 @@
+var baseUrl = "http://boxilearning.000webhostapp.com/backend_restapi/backend_restapi/index.php";
+var login = "/auth/login";
+var logout = "/auth/logout";
+
+var addStudent = "/student/create";
+var updateStudent = "/student/update/";
+var deleteStudent = "/student/delete/";
+var getStudent = "/student/detail/";
+var getallStudent = "/student";
+var getStudentAvailable = "/student/available"
+var kickStudent = "/student/kick/"
+var assignStudent = "/student/assign/"
+
+var addClass = "/class_controller/create";
+var updateClass = "/class_controller/update/";
+var deleteClass = "/class_controller/delete/";
+var getClass = "/class_controller/detail/";
+var getallClass = "/class_controller";
+var getClasslist = "/class_controller/class_list/";
